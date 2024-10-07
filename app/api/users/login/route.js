cea5db1502b9adb9781ae36d82122fb82bde20c5
michael seaman <mseaman26@gmail.com>
2024-10-07 13:04:33 -0700

    using next response

--- app/api/users/login/route.js
+++ app/api/users/login/route.js
@@ -3,6 +3,7 @@ import { connectMongoDB } from "@/utils/mongodb";
 import User from "@/models/User";
 import Auth from "@/utils/auth"; // Assuming you have a utility for signing tokens
 import { setCorsHeaders } from "@/utils/helpers";
+import { NextResponse } from "next/server";
 
 export async function OPTIONS() {
     // Respond to preflight request with CORS headers
@@ -30,13 +31,13 @@ export async function POST(request) {
         // Find the user by email
         const user = await User.findOne({ email: email.toLowerCase() });
         if (!user) {
-        return new Response(JSON.stringify({ success: false, errorMessage: "Invalid email or password" }), { status: 401, headers: setCorsHeaders() });
+        return NextResponse.json({ success: false, errorMessage: "Invalid email or password" }, { status: 401, headers: setCorsHeaders() });
         }
 
         // Check if the provided password is valid
         const isPasswordValid = await user.isPasswordValid(password);
         if (!isPasswordValid) {
-        return new Response(JSON.stringify({ success: false, errorMessage: "Invalid email or password" }), { status: 401, headers: setCorsHeaders() });
+        return NextResponse.json({ success: false, errorMessage: "Invalid email or password" }, { status: 401, headers: setCorsHeaders() });
         }
 
         // If successful, sign a token and return user data (excluding password)
@@ -45,6 +46,6 @@ export async function POST(request) {
         return new Response(JSON.stringify({ success: true, data: { user: userData, token } }), { status: 200 });
     } catch (error) {
         console.error("Error during login: ", error);
-        return new Response(JSON.stringify({ success: false, errorMessage: "Internal server error" }), { status: 500, headers: setCorsHeaders() });
+        return NextResponse.json({ success: false, errorMessage: "Server error. Please try again later" }, { status: 500, headers: setCorsHeaders() });
     }
 }
