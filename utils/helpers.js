c80640ae67d56dfff18227609069f58150bd2c18
michael seaman <mseaman26@gmail.com>
2024-10-21 14:35:13 -0700

    headers specifying allowed origin

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 export const setCorsHeaders = () => {
     return {
-      "Access-Control-Allow-Origin": "*", 
+      "Access-Control-Allow-Origin": 'https://lotto-picker-rn-vite.vercel.app', 
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     };
